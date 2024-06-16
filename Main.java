82fcf7b7e723c2ac7b3b2303015afc6f44d942e2
lahiru-prasnga <lahiruprasangasrimal@gmail.com>
2024-06-16 17:36:21 +0530

    Add code with errors

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Java program is working");
         System.out.println("Test 01");
-        System.out.println("Test 02");
+        System.out.printl("Test 02");
     }
 }
